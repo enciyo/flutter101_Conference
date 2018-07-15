5c2b923f81a3d59bf26ae96b7a150b26abe420a5
Enciyo <enc1y0@hotmail.com>
2018-07-15 10:09:59 +0300

    Listview & ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,25 @@ void main() {
       ),
     );
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Column & Row"),
+        title: new Text("Listview & ListTile"),
+        centerTitle: true,
       ),
-      body: new Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: new ListView(
         children: <Widget>[
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text("item"),
-              new Text("item"),
-              new Text("item"),
-              new Text("item"),
-              new Text("item"),
-            ],
-          ),
-
+          new ListTile(
+            title: new Text("Item",textAlign: TextAlign.center,),
+            leading: new Icon(Icons.account_circle),//left side icon
+            trailing: new IconButton(icon: new Icon(Icons.refresh), onPressed: (){}),//Righs side icon
+            subtitle: new Icon(Icons.linear_scale),//under side icon
+            onTap: (){print("onTap");},
+            onLongPress: (){print("onLongPress");},
+          )
         ],
       ),
     );
