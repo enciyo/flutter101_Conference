d88ee7df59f1f54cd17df4366de27256a539e35e
Enciyo <enc1y0@hotmail.com>
2018-07-15 09:30:00 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      home: new Center(
+        child: new Text("Hello flutter"),
+      ),
+    )
+  );
+}
