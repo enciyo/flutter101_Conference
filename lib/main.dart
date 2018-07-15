8c675bf98cfa7676a8baab9ee572d34d212cff1d
Enciyo <enc1y0@hotmail.com>
2018-07-15 09:46:28 +0300

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,23 @@ void main() {
     );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   String name="Passive";
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new MaterialButton(onPressed: (){name="Active";},child: new Text(name),),
-      //Doesnt change
+      body: new MaterialButton(onPressed: (){
+        // if we want change text name, we must use setState method
+        //This method will reload the Widget
+        setState(() {
+          this.name="Active";
+        });
+      },child: new Text(name),),
     );
   }
 }
