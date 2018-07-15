bab21a0855407784470474f73e6251f99a165c6b
Enciyo <enc1y0@hotmail.com>
2018-07-15 09:37:29 +0300

    Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new MaterialApp(
-      home: new Center(
-        child: new Text("Hello flutter"),
+      home: new MyApp(),
       ),
-    )
-  );
+    );
+}
+
+class MyApp extends StatelessWidget {
+  String name="Passive";
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new MaterialButton(onPressed: (){name="Active";},child: new Text(name),),
+      //Doesnt change
+    );
+  }
 }
